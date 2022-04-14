e373e59fd6375dd1bcd57407178531bbd0f03d3f
qixui_li <402496187@qq.com>
2022-04-14 17:11:22 +0800

    gui

--- use_gui/picture_gui.py
+++ use_gui/picture_gui.py
@@ -7,46 +7,6 @@ import os
 from predict import predict
 
 
-# class inference_thread(QThread):
-# inference 线程
-# statusSignal = pyqtSignal(list)
-
-# def __init__(self, parent=None):
-#     super(inference_thread, self).__init__(parent)
-# self._filenames = filenames
-# self.vehicle_detector = vehicle_detector
-
-# def run(self):
-#     for idx, filename in enumerate(self._filenames):
-#         img_size, image_np = input_for_detecion(filename)
-#         file_type = filename.split('/')[-1].split('.')[-1]
-#         vehicle_box, vehicle_predict_lab = self.vehicle_detector.inference(image_np, img_size)
-#         if vehicle_box:
-#             image_result = draw_predictions(image_np, img_size, vehicle_box, vehicle_predict_lab)
-#             image_result.save(filename.split(file_type)[0] + '_result.jpg')
-#             # signal runing:car detected
-#             self.statusSignal.emit([1, filename, idx, image_result])
-#         else:
-#             # signal runing:no car detected
-#             self.statusSignal.emit([-1, filename, idx])
-#     # signal finished
-#     self.statusSignal.emit([0])
-
-
-# class load_models_thread(QThread):
-# 导入model线程
-# finishSignal = pyqtSignal(predict.look_image())
-
-# def __init__(self, parent=None):
-#     super(load_models_thread, self).__init__(parent)
-
-# def run(self):
-#     # get models config
-#     (DETECT_CONFIG, CLASSIFY_CONFIG) = get_models_config()
-#     vehicle_detector = VehileDetectionModel(get_detector_mode(), detect_config=DETECT_CONFIG,
-#                                             classify_config=CLASSIFY_CONFIG)
-#     self.finishSignal.emit(vehicle_detector)
-
 
 class PictureGui(QWidget):
     url = ''
@@ -58,24 +18,6 @@ class PictureGui(QWidget):
         self.setUI()
 
 
-    # def setlog(LOG_PATH):
-    #     LOG_FORMAT = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(message)s"
-    #     DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
-    #     logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
-    #
-    #     console = logging.StreamHandler()
-    #     console.setLevel(logging.INFO)
-    #     formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-    #     console.setFormatter(formatter)
-    #     logging.getLogger('').addHandler(console)
-
-    # def get_log_dir():
-    #     # path to save logs
-    #     if not os.path.exists('./log'):
-    #         os.mkdir('./log')
-    #     log_name = datetime.now().strftime("log-%Y%m%d-%H%M%S.txt")
-    #     LOG_PATH = './log/' + log_name
-    #     return LOG_PATH
 
 
     def setUI(self):
@@ -85,12 +27,12 @@ class PictureGui(QWidget):
         self.setToolTip("<b>看图说话</b>")
 
         # add button to GUI
-        btn_load = QPushButton("导入照片 开始描述", self)
+        btn_load = QPushButton("导入照片", self)
         btn_load.setToolTip("点击导入需要描述的图片")
         btn_load.setDisabled(False)
-        # btn_inference = QPushButton("描述图片", self)
-        # btn_inference.setToolTip("点击开始描述导入的图片")
-        # btn_inference.setDisabled(True)
+        btn_inference = QPushButton("描述图片", self)
+        btn_inference.setToolTip("点击开始描述导入的图片")
+        btn_inference.setDisabled(True)
 
         # add checkbox to GUI
         cbx_auto_open = QCheckBox("自动打开", self)
@@ -106,7 +48,7 @@ class PictureGui(QWidget):
         # horizon
         h_box = QHBoxLayout()
         h_box.addWidget(btn_load)
-        # h_box.addWidget(btn_inference)
+        h_box.addWidget(btn_inference)
         h_box.addWidget(cbx_auto_open)
 
         # vertical
@@ -121,22 +63,23 @@ class PictureGui(QWidget):
         # 设置导入图片按钮点击事件
         btn_load.clicked.connect(self.load_image)
         # 设置描述图片事件
-        # btn_inference.clicked.connect(predict.look_image(os.system('start explorer ' + self.url)))
-        # self.btn_inference = btn_inference
+        btn_inference.clicked.connect(self.describe_img)
+        self.btn_inference = btn_inference
         self.btn_load = btn_load
         self.cbx_auto_open = cbx_auto_open
         self.center()
         self.show()
-        # img = self.load_image()
-
-        # setlog(get_log_dir())
-        # self.debug_info.addItem('Status: BUSY : Set log dir:{}'.format(get_log_dir().split('/')[-1]))
         self.debug_info.addItem('Status: BUSY : Please choice you picture...')
 
-    # def load_models_end(self, result):
-    #     self.vehicle_detector = result
-    #     self.btn_load.setDisabled(False)
-    #     self.debug_info.addItem('Status: IDLE : Load detection model done!')
+
+    def describe_img(self):
+        self.debug_info.addItem('Status: BUSY :The program is processing images,please wait... ')
+        print("input img "+self.url)
+        res = predict.look_image(open(self.url,encoding='utf-8',mode='r'))
+        self.debug_info.addItem('******describe '+res+'******')
+        self.debug_info.addItem('Status:IDLE :images finish !')
+
+
 
     def center(self):
         qr = self.frameGeometry()
@@ -153,21 +96,16 @@ class PictureGui(QWidget):
 
         if not filenames:
             return
-        # self._filenames = []
-        # 得到输入图片路径名
-        # print(filenames)
         self.url = filenames[0]
         # filenames list 类型
         self.path = filenames[0].split(filenames[0].split('/')[-1])[0]
-        # self.im_num = len(filenames)
         for filename in filenames:
             # 图片类型辨别
             file_type = filename.split('/')[-1].split('.')[-1]
             if file_type in ['jpg', 'jpeg', 'png']:
                 self.debug_info.addItem('Status: BUSY :Load image: ' + filename.split('/')[-1])
                 # 正在进行图片类型的判断，未成功时，不会显示 描述图片按钮
-                # self.btn_inference.setDisabled(False)
-                # self._filenames.append(filename)
+                self.btn_inference.setDisabled(False)
             elif file_type:
                 # QMessageBox.information(self, "提示", "不支持的格式")
                 self.debug_info.addItem('Not support file type of loaded file:' + filename.split('/')[-1])
@@ -175,9 +113,6 @@ class PictureGui(QWidget):
         self.debug_info.addItem('Status: IDLE :Load images done!')
         # 文件路径标准化转化
         self.url = self.url.replace('/', '\\')
-        print(self.url)
-        # todo 使用文件路径，得到图片文件，作为参数传入
-        # self.debug_info.addItem(predict.look_image(os.system("start explorer " + self.url)))
 
     # def inference(self):
     #     # update gui
